6f6f36eb3f6f22736ea50d95c7a19e509ecfea09
imran-de <stimran4405@gmail.com>
2021-11-29 20:36:35 +0600

    data add

--- src/pages/Home/Projects/Projects.js
+++ src/pages/Home/Projects/Projects.js
@@ -23,7 +23,7 @@ const Projects = () => {
                                 <Card.Img variant="top" src="https://i.ibb.co/nCyGrJk/drone.png" />
                             </div>
                             <Card.Body>
-                                <Card.Title className='text-uppercase'>MERN STACK Project</Card.Title>
+                                <Card.Title className='text-uppercase'>Drone</Card.Title>
                                 <Card.Text>
                                     <span className='fw-bolder'>Technology: </span>
                                     React JS, Node JS, MongoDB, Express JS
@@ -31,94 +31,94 @@ const Projects = () => {
                             </Card.Body>
                             <Card.Footer className='d-flex justify-content-between border-0'>
                                 <Link to="/details">Details</Link>
-                                <a href="#github" target='_blank'>Code</a>
-                                <a href="#Live" target='_blank'>Live</a>
+                                <a href="https://github.com/imran-de/drone-client" rel='noreferrer' target='_blank'>Code</a>
+                                <a href="https://drone-45254.web.app/" rel='noreferrer' target='_blank'>Live</a>
                             </Card.Footer>
                         </Card>
                     </Col>
                     <Col>
                         <Card className='h-100'>
                             <div className='project-img'>
-                                <Card.Img variant="top" src="https://i.ibb.co/nCyGrJk/drone.png" />
+                                <Card.Img variant="top" src="https://i.ibb.co/QbqD403/vacation-planner.png" />
                             </div>
                             <Card.Body>
-                                <Card.Title className='text-uppercase'>MERN STACK Project</Card.Title>
+                                <Card.Title className='text-uppercase'>Vacation planner</Card.Title>
                                 <Card.Text>
                                     <span className='fw-bolder'>Technology: </span>
-                                    React JS, Node JS, MongoDB, Express JS
+                                    React JS, Node JS, MongoDB, Express JS etc
                                 </Card.Text>
                             </Card.Body>
                             <Card.Footer className='d-flex justify-content-between border-0'>
                                 <Link to="/details">Details</Link>
-                                <a href="#github" target='_blank'>Code</a>
-                                <a href="#Live" target='_blank'>Live</a>
+                                <a href="https://github.com/imran-de/vacation-planner-client" rel='noreferrer' target='_blank'>Code</a>
+                                <a href="https://vacation-planner-916b4.web.app/" rel='noreferrer' target='_blank'>Live</a>
                             </Card.Footer>
                         </Card>
                     </Col>
                     <Col>
                         <Card className='h-100'>
                             <div className='project-img'>
-                                <Card.Img variant="top" src="https://i.ibb.co/nCyGrJk/drone.png" />
+                                <Card.Img variant="top" src="https://i.ibb.co/w7WMfkz/healthcare.png" />
                             </div>
                             <Card.Body>
-                                <Card.Title className='text-uppercase'>MERN STACK Project</Card.Title>
+                                <Card.Title className='text-uppercase'>Medi-Health care</Card.Title>
                                 <Card.Text>
                                     <span className='fw-bolder'>Technology: </span>
-                                    React JS, Node JS, MongoDB, Express JS
+                                    React JS, Bootstrap, React-router-dom etc.
                                 </Card.Text>
                             </Card.Body>
                             <Card.Footer className='d-flex justify-content-between border-0'>
                                 <Link to="/details">Details</Link>
-                                <a href="#github" target='_blank'>Code</a>
-                                <a href="#Live" target='_blank'>Live</a>
+                                <a href="https://github.com/imran-de/medi-healt-care" rel='noreferrer' target='_blank'>Code</a>
+                                <a href="https://health-care-react.web.app/" target='_blank' rel="noreferrer">Live</a>
                             </Card.Footer>
                         </Card>
                     </Col>
                     <Col>
                         <Card className='h-100'>
                             <div className='project-img'>
-                                <Card.Img variant="top" src="https://i.ibb.co/nCyGrJk/drone.png" />
+                                <Card.Img variant="top" src="https://i.ibb.co/SVQH0Gx/club.png" />
                             </div>
                             <Card.Body>
-                                <Card.Title className='text-uppercase'>MERN STACK Project</Card.Title>
+                                <Card.Title className='text-uppercase'>Club Landing page</Card.Title>
                                 <Card.Text>
                                     <span className='fw-bolder'>Technology: </span>
-                                    React JS, Node JS, MongoDB, Express JS
+                                    HTML, CSS
                                 </Card.Text>
                             </Card.Body>
                             <Card.Footer className='d-flex justify-content-between border-0'>
                                 <Link to="/details">Details</Link>
-                                <a href="#github" target='_blank'>Code</a>
-                                <a href="#Live" target='_blank'>Live</a>
+                                <a href="https://github.com/imran-de/Football" rel='noreferrer' target='_blank'>Code</a>
+                                <a href="https://imran-de.github.io/Football/" rel='noreferrer' target='_blank'>Live</a>
                             </Card.Footer>
                         </Card>
                     </Col>
                     <Col>
                         <Card className='h-100'>
                             <div className='project-img'>
-                                <Card.Img variant="top" src="https://i.ibb.co/nCyGrJk/drone.png" />
+                                <Card.Img variant="top" src="https://i.ibb.co/bKWc948/cbr.png" />
                             </div>
                             <Card.Body>
-                                <Card.Title className='text-uppercase'>MERN STACK Project</Card.Title>
+                                <Card.Title className='text-uppercase'>Honda Landing page</Card.Title>
                                 <Card.Text>
                                     <span className='fw-bolder'>Technology: </span>
-                                    React JS, Node JS, MongoDB, Express JS
+                                    HTML, CSS, Bootstrap etc
                                 </Card.Text>
                             </Card.Body>
                             <Card.Footer className='d-flex justify-content-between border-0'>
                                 <Link to="/details">Details</Link>
-                                <a href="#github" target='_blank'>Code</a>
-                                <a href="#Live" target='_blank'>Live</a>
+                                <a href="https://github.com/imran-de/honda-cbr" rel='noreferrer' target='_blank'>Code</a>
+                                <a href="https://honda-cbr-imran.netlify.app/" rel='noreferrer' target='_blank'>Live</a>
                             </Card.Footer>
                         </Card>
                     </Col>
                     <Col>
                         <Card className='h-100'>
                             <div className='project-img'>
-                                <Card.Img variant="top" src="https://i.ibb.co/nCyGrJk/drone.png" />
+                                <Card.Img variant="top" src="https://i.ibb.co/7C5H6gm/doctor.png" />
                             </div>
                             <Card.Body>
-                                <Card.Title className='text-uppercase'>MERN STACK Project</Card.Title>
+                                <Card.Title className='text-uppercase'>Doctor Portal</Card.Title>
                                 <Card.Text>
                                     <span className='fw-bolder'>Technology: </span>
                                     React JS, Node JS, MongoDB, Express JS
@@ -126,8 +126,8 @@ const Projects = () => {
                             </Card.Body>
                             <Card.Footer className='d-flex justify-content-between border-0'>
                                 <Link to="/details">Details</Link>
-                                <a href="#github" target='_blank'>Code</a>
-                                <a href="#Live" target='_blank'>Live</a>
+                                <a href="https://github.com/imran-de/Doctors-portal-client" rel='noreferrer' target='_blank'>Code</a>
+                                <a href="https://simple-firebase-auth-prac.web.app/" rel='noreferrer' target='_blank'>Live</a>
                             </Card.Footer>
                         </Card>
                     </Col>
